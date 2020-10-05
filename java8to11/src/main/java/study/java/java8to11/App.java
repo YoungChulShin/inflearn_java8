6f894e01c471b160138965c3854d61bbe096f35b
YoungChulShin <go1323@gmail.com>
2020-10-05 22:30:18 +0900

    optional api

--- java8to11/src/main/java/study/java/java8to11/App.java
+++ java8to11/src/main/java/study/java/java8to11/App.java
@@ -12,12 +12,21 @@ public class App {
 
        List<OnlineClass> springClasses = new ArrayList<>();
        springClasses.add(new OnlineClass(1, "spring boot", true));
-       springClasses.add(new OnlineClass(2, "spring data jpa", true));
-       springClasses.add(new OnlineClass(3, "spring mvc", false));
-       springClasses.add(new OnlineClass(4, "spring core", false));
        springClasses.add(new OnlineClass(5, "rest api development", false));
 
-       OnlineClass spring_boot = new OnlineClass(1, "spring boot", true);
-       spring_boot.pro
+        Optional<OnlineClass> onlineClasses = springClasses.stream()
+                .filter(oc -> oc.getTitle().startsWith("spring"))
+                .findFirst();
+
+        Optional<Integer> optionalInteger = onlineClasses.map(o -> o.getId());
+
+        //onlineClasses.ifPresent(oc -> System.out.println(oc.getTitle()));
+
+        OnlineClass onlineClass = onlineClasses.orElse(createNewClass());
+    }
+
+    private static OnlineClass createNewClass() {
+        System.out.println("New Class");
+        return new OnlineClass(10, "New Class", false);
     }
 }
