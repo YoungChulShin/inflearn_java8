8ebf4783a625a3224ec31ae36980a954a04fbf87
YoungChulShin <go1323@gmail.com>
2020-10-06 23:40:47 +0900

    DateTime

--- java8to11/src/main/java/study/java/java8to11/App.java
+++ java8to11/src/main/java/study/java/java8to11/App.java
@@ -1,6 +1,9 @@
 package study.java.java8to11;
 
 
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 import java.util.*;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
@@ -10,23 +13,12 @@ public class App {
 
     public static void main(String[] args) {
 
-       List<OnlineClass> springClasses = new ArrayList<>();
-       springClasses.add(new OnlineClass(1, "spring boot", true));
-       springClasses.add(new OnlineClass(5, "rest api development", false));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MM/dd/yyyy");
 
-        Optional<OnlineClass> onlineClasses = springClasses.stream()
-                .filter(oc -> oc.getTitle().startsWith("spring"))
-                .findFirst();
+        LocalDate parse = LocalDate.parse("09/06/1984", formatter);
+        System.out.println(parse);
 
-        Optional<Integer> optionalInteger = onlineClasses.map(o -> o.getId());
+        Instant instant = Instant.now();
 
-        //onlineClasses.ifPresent(oc -> System.out.println(oc.getTitle()));
-
-        OnlineClass onlineClass = onlineClasses.orElse(createNewClass());
-    }
-
-    private static OnlineClass createNewClass() {
-        System.out.println("New Class");
-        return new OnlineClass(10, "New Class", false);
     }
 }
